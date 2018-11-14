6431cc464bf0542d0382f3dbec7a79f928c018ce
BLBaylis <bradleybaylis@hotmail.com>
2018-11-14 23:26:30 +0000

    WinCheck method working for lineLength = 3

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -11,12 +11,9 @@ class Game extends React.Component {
   };
 
   handleClick = (squareNo, board) => {   
-    //FIX USER NOT BEING ABLE TO WIN / MOVE 9 NOT WORKING
-    console.log(board[squareNo]);
     if (board[squareNo] || this.winCheck(squareNo, board)) {
       return;
     }
-    console.log("passed");
     this.setState(prevState => {
       const boardClone = prevState.board.slice();
       if (prevState.gameLog[prevState.turnNo + 1]){
@@ -36,28 +33,28 @@ class Game extends React.Component {
 
     const horizontalLineChecker = (squareNumber, gameBoard, lineLength = 3) => {
       const rowNum = Math.floor(squareNumber / lineLength);
-      const arr = Array(lineLength).fill().map((x, index) => index * rowNum);
-      return arr.every(x => gameBoard[x] === gameBoard[rowNum]);
+      const arr = Array(lineLength).fill().map((x, index) => index + (rowNum * 3));
+      return arr.every(x => gameBoard[x] === gameBoard[rowNum * lineLength] && gameBoard[x]);
     };
 
     const verticalLineChecker = (squareNumber, gameBoard, lineLength = 3) => {
       const colNum = squareNumber % lineLength;
       const arr = Array(lineLength).fill().map((x, index) => (index * lineLength) + colNum);
-      return arr.every(x => gameBoard[x] === gameBoard[colNum]);
+      return arr.every(x => gameBoard[x] === gameBoard[colNum] && gameBoard[x]);
     };
 
     const diagonalLineChecker = (gameBoard, lineLength = 3) => {
       const diag1 = Array(lineLength).fill().map((x, index) => (index * lineLength) + index);
-      const diag2 = Array(lineLength).fill().map((x, index) => (index * lineLength) - index);
-      return [diag1, diag2].some(x => x.every(x => gameBoard[x] === gameBoard[4]));
+      const diag2 = Array(lineLength).fill().map((x, index) => ((index + 1) * lineLength) - (index + 1));
+      return [diag1, diag2].some(x => x.every(x => gameBoard[x] === gameBoard[4] && gameBoard[x]));
     };
 
     if (lineLength % 2 === 0 || lineLength < 3) {
         throw new Error("LineLength needs to be an odd number bigger than 3");
     }
     const n = ((lineLength - 1) / 2);
-    const center = (lineLength * n) - n;
-    if (horizontalLineChecker(center, gameBoard, lineLength) || verticalLineChecker(center, gameBoard, lineLength) || diagonalLineChecker(center, gameBoard, lineLength)) {
+    const center = (lineLength * n) + n;
+    if (horizontalLineChecker(center, gameBoard, lineLength) || verticalLineChecker(center, gameBoard, lineLength) || diagonalLineChecker(gameBoard, lineLength)) {
         return true;
     }
     if (horizontalLineChecker(0, gameBoard, lineLength) || verticalLineChecker(0, gameBoard, lineLength)) {
@@ -72,7 +69,7 @@ class Game extends React.Component {
 
   restart = () => {
     this.setState({
-      board: [Array(9).fill(null)],
+      board: Array(9).fill(null),
       turnNo: 0,
       userTurn: true,
       gameLog: [{board: Array(9).fill(null), turnNo : 0, userTurn: true}]
