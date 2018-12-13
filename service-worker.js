8d6cd96edbb3e3c4cb4e37842a67bfb8bfa73430
BLBaylis <bradleybaylis@hotmail.com>
2018-12-13 11:38:07 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "blbaylis.github.io/tic-tac-toe-v2/precache-manifest.bc83df2d99379266230ad3c987023559.js"
+  "blbaylis.github.io/tic-tac-toe-v2//precache-manifest.ebdfd03aee8ce3f28df760c99b8f84a1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("blbaylis.github.io/tic-tac-toe-v2/index.html", {
+workbox.routing.registerNavigationRoute("blbaylis.github.io/tic-tac-toe-v2//index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
